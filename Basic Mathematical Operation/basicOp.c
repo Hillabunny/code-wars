8f698120159c924ca04e5771d4fad0e400b0a41f
Hillabunny <98918650+Hillabunny@users.noreply.github.com>
2025-05-15 12:39:10 -0400

    Create basicOp.c
    Code Kata

---
+++ Basic Mathematical Operation/basicOp.c
@@ -0,0 +1,18 @@
+int basicOp(char op, int val1, int val2) {
+  switch (op) {
+    case '+':
+      return val1 + val2;
+  break;
+    case '-':
+      return val1 - val2;
+  break;
+    case '*':
+      return val1 * val2;
+  break;
+  case'/':
+    return val1 / val2;
+  break;
+  default:
+  return 0;
+      }
+}
